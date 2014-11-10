193dd2e616e686dd9a2840745609644b97a78d27
Anuvis Mejia <ameji012@gmail.com>
2014-11-10 18:37:55 -0500

    htmlcss

--- server_me.rb
+++ server_me.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  File.read(File.join('public', 'amejia2.html'))
+  File.read(File.join('public', 'home.html'))
+end
+get '/projects' do
+  File.read(File.join('public', 'resume.html'))
+end
+get '/resume' do
+  File.read(File.join('public', 'projects.html'))
 end
