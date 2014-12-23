4562513c3116c1549ad7edbcbe4b7afbbf04d730
Anuvis Mejia <ameji012@gmail.com>
2014-12-23 10:35:29 -0500

    edit in app.rb

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require 'sinatra/base'
 require 'sinatra/assetpack'
 require 'sass'
+require 'rack'
 
 class App < Sinatra::Base
   register Sinatra::AssetPack
   assets do
     css :main, [
-     '/css/reset.css',
      '/css/*.css'
     ]
     css_compression :sass
