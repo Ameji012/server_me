5bee1973df51d5ab54535070033d4059f0a996d2
Anuvis Mejia <ameji012@gmail.com>
2015-05-09 11:03:04 -0400

    updates with moves

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,40 @@
 //= require jquery.fullPage.js
 //= require jquery.validate.min
 //= require jquery-validate.bootstrap-tooltip
+//= require owl.carousel.js
+ 
+$(function() { 
+  $(".education-carousel").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+  $(".skills-carousel").owlCarousel()
+});
+
+
+// Scroll Function
+// $(function() {
+//   $('a[href*=#]:not([href=#])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+// });
