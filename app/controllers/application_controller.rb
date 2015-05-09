16111c0c69cc176c339f348144be1a14e3b74c39
Anuvis Mejia <ameji012@gmail.com>
2015-05-09 01:10:01 -0400

    config.ru mod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  after_filter :set_access_control_headers
-
-  def set_access_control_headers 
-    headers['Access-Control-Allow-Origin'] = 'http://www.anuvism.com'  
-    headers['Access-Control-Request-Method'] = '*'
-  end
 end
