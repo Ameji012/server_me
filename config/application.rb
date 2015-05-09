7e661da48185c071ce8045a828c4fc3ac153e882
Anuvis Mejia <ameji012@gmail.com>
2015-05-09 01:15:14 -0400

    rack in app.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
+require 'rack/cors'
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -29,7 +31,12 @@ module Mywebsite
     end
 
     config.action_controller.action_on_unpermitted_parameters = :raise
-    
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins 'http://www.anuvism.com'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
